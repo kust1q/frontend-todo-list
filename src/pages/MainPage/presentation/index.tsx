f732dac97a2334d91aa0ab2bfd1aca079bd0288d
aromanov <aromanov@inno.tech>
2022-12-05 23:44:36 +0400

    add k8s deployment

--- src/pages/MainPage/presentation/index.tsx
+++ src/pages/MainPage/presentation/index.tsx
@@ -4,22 +4,22 @@ import NewItem from "../../../components/NewItem";
 import TodoItem from "../../../components/TodoItem";
 
 type ItemList = {
-    items: Item[],
-    createNewItem: Function
-    removeItem: Function
-    loadItems: Function
+  items: Item[],
+  createNewItem: Function
+  removeItem: Function
+  loadItems: Function
 }
 
 const Presentation: FC<ItemList> = ({items, createNewItem, removeItem, loadItems}) => {
-    useEffect(() => {
-        loadItems()
-    }, []);
-    return (
-        <div className="container-fluid vh-100">
-            <NewItem createNewItem={createNewItem}/>
-            {items.map(item => <TodoItem key={item.uid} item={item} removeItem={removeItem}/>)}
-        </div>
-    );
+  useEffect(() => {
+    loadItems()
+  }, [items]);
+  return (
+      <div className="container-fluid vh-100">
+        <NewItem createNewItem={createNewItem}/>
+        {items.map(item => <TodoItem key={item.uid} item={item} removeItem={removeItem}/>)}
+      </div>
+  );
 }
 
 export default Presentation;
