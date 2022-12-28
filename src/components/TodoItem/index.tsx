aa7deac4200d55c507af07da64aaba6303f91c7f
aromanov <aromanov@inno.tech>
2022-12-29 01:55:55 +0400

    add css locators

--- src/components/TodoItem/index.tsx
+++ src/components/TodoItem/index.tsx
@@ -12,10 +12,10 @@ const TodoItem: FC<Props> = ({item: {text, uid}, removeItem}) => {
         <div className="row justify-content-center" data-id={`todo-item-row-${uid}`}>
             <div className="col-lg-4 item-row">
                 <div className="d-flex justify-content-between">
-                    <span className="mx-3" data-it={`todo-item-text-span-${uid}`}>{text}</span>
+                    <span className="mx-3" data-id={`todo-item-text-span-${uid}`}>{text}</span>
                     <span>&nbsp;</span>
                     <button type="button"
-                            data-it={`todo-item-remove-button-${uid}`}
+                            data-id={`todo-item-remove-button-${uid}`}
                             className="btn btn-outline-danger"
                             onClick={() => removeItem(uid)}>
                         <i className="bi bi-x-circle"/>
